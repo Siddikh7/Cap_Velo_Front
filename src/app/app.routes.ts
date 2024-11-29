570922bb0cb497135275fefbd5fee9741f044c4f
JaafarG <jghiffi@mial.com>
2024-11-29 15:33:23 +0100

    Rajout de la carte de base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'velos', loadComponent: () => import('./components/table-test/table-test.component').then(m => m.TableTestComponent) },
-  { path: '', redirectTo: '/velos', pathMatch: 'full' }, // Redirection vers 'velos' par défaut
-  { path: '**', redirectTo: '/velos' } // Redirection pour les routes non trouvées
+  { path: 'reservations', loadComponent: () => import('./components/reservation/reservation.component').then(m => m.ReservationComponent) },
+{ path: 'carte', loadComponent: () => import('./components/map/map.component').then(m => m.MapComponent) },
+  { path: '', redirectTo: '/velos', pathMatch: 'full' },
+  { path: '**', redirectTo: '/velos' }
 ];
---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+  private map!: L.Map;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [47.393581, 0.675309],
+      zoom: 13
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+}
