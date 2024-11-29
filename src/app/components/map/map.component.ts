a2cc904003f956a772ccfcabe6d434141cf37045
JaafarG <jghiffi@mial.com>
2024-11-29 16:02:52 +0100

    Rajout de pins

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { VeloService } from '../../services/velo.service';
+import { VeloModel } from '../../models/velo.model';
 
 @Component({
   selector: 'app-map',
@@ -10,19 +12,43 @@ import * as L from 'leaflet';
 export class MapComponent implements OnInit {
   private map!: L.Map;
 
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [47.393581, 0.675309],
-      zoom: 13
-    });
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(this.map);
-  }
+  private customIcon = L.icon({
+    iconUrl: 'assets/pin.png', 
+    iconSize: [38, 38], 
+    iconAnchor: [19, 38],
+    popupAnchor: [0, -38]
+  });
+
+  constructor(private veloService: VeloService) {}
 
   ngOnInit(): void {
     this.initMap();
+
+    // Récupérez les vélos et ajoutez leurs points sur la carte
+    this.veloService.showAll().subscribe((velos: VeloModel[]) => {
+      velos.forEach(velo => {
+        // Transformez pointGeo en latitude et longitude
+        const [latitude, longitude] = velo.pointGeo.split(' ').map(coord => parseFloat(coord));
+
+        if (!isNaN(latitude) && !isNaN(longitude)) {
+          // Ajoutez un marqueur avec l'icône personnalisée
+          L.marker([latitude, longitude], { icon: this.customIcon })
+            .bindPopup(`<b>${velo.nom}</b><br>${velo.description}<br>Quantité: ${velo.quantite}`)
+            .addTo(this.map);
+        } else {
+          console.warn(`Coordonnées invalides pour le vélo: ${velo.nom}`);
+        }
+      });
+    });
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([47.3946, 0.6848], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    }).addTo(this.map);
   }
 }
